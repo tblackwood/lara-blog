d3002ef0b030037a9267a2bf77e3a57e03bad248
Tarik V <tarasv290@gmail.com>
2018-04-24 10:58:08 +0300

    Create admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware'=>['auth']], function (){
+    Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+});
